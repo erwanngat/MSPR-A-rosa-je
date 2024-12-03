ec287edd5a894cd2076206734eb210b0c635255e
erwanngat <ewnran69@yahoo.fr>
2024-12-03 17:04:57 +0100

    test

--- back/routes/api.php
+++ back/routes/api.php
@@ -5,8 +5,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\UserController;
 use App\Http\Controllers\API\AuthController;
 
-Route::apiResource('users', UserController::class);
 
+Route::apiResource('users', UserController::class);
 Route::post('register', [AuthController::class, 'register']);
 Route::post('login', [AuthController::class, 'login']);
 
