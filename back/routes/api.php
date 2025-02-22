62399be7804e5aa4053813f6556b2f6e0dd25c41
erwanngat <ewnran69@yahoo.fr>
2025-02-22 23:15:28 +0100

    fix

--- back/routes/api.php
+++ back/routes/api.php
@@ -16,7 +16,7 @@ Route::withoutMiddleware([VerifyCsrfToken::class])->group(function () {
     Route::post('login', [AuthController::class, 'login']);
 });
 
-//Route::middleware('auth:sanctum')->group(function () {
+Route::middleware('auth:sanctum')->group(function () {
     Route::apiResource('users', UserController::class);
     Route::apiResource('plantes', PlanteController::class);
     Route::apiResource('comments', CommentController::class);
@@ -24,4 +24,4 @@ Route::withoutMiddleware([VerifyCsrfToken::class])->group(function () {
     Route::apiResource('addresses', AddressController::class);
     Route::get('plantes/{id}/comments', [PlanteController::class, 'getPlanteComments']);
     Route::get('plantes/{id}/reservations', [PlanteController::class, 'getPlanteReservation']);
-//});
+});
