dfcd0f63de088fe5b3c2c31a4b0b94fb78ba282a
erwanngat <ewnran69@yahoo.fr>
2025-02-26 09:30:36 +0100

    add default image

--- back/database/seeders/PlanteSeeder.php
+++ back/database/seeders/PlanteSeeder.php
@@ -16,18 +16,21 @@ class PlanteSeeder extends Seeder
         DB::table('plantes')->insert([
             'name' => 'plante 1',
             'description' => 'Plante description 1',
+            'image' => 'http://localhost:8080/storage/plantes/default.webp',
             'address_id' => 1,
             'user_id' => 1,
         ]);
         DB::table('plantes')->insert([
             'name' => 'plante 2',
             'description' => 'Plante description 2',
+            'image' => 'http://localhost:8080/storage/plantes/default.webp',
             'address_id' => 1,
             'user_id' => 1,
         ]);
         DB::table('plantes')->insert([
             'name' => 'plante 3',
             'description' => 'Plante description 3',
+            'image' => 'http://localhost:8080/storage/plantes/default.webp',
             'address_id' => 2,
             'user_id' => 2,
         ]);
